c3841ec45d08ad769b6ed33b6f60e314379f5b87
yuanzi13 <154969939+yuanzi13@users.noreply.github.com>
2026-08-02 11:36:20 +0800

    Update importance_analysis.py

--- experiments/ternary/loso/importance_analysis.py
+++ experiments/ternary/loso/importance_analysis.py
@@ -12,6 +12,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader, random_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import (
     confusion_matrix, accuracy_score, precision_score,
     recall_score, f1_score, roc_auc_score, roc_curve
